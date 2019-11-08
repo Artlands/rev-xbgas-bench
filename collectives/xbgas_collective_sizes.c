bb0192250bf84c0f92b240498c2b7b3b889d9de2
Brody Williams <brody.williams@ttu.edu>
2019-11-08 17:09:33 -0600

    add test file

---
+++ collectives/xbgas_collective_sizes.c
@@ -0,0 +1,348 @@
+#include "xbrtime.h"
+
+#define FLAG 0xC000000000000000ull
+
+void small_broadcast()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 256;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    if(my_pe == 0)
+    {
+        for(int i = 0; i < nelems; i++)
+        {
+            message[i] = my_pe;
+        }
+    }
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_broadcast_tree(message, message, nelems, 1, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void large_broadcast()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 25600;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    if(my_pe == 0)
+    {
+        for(int i = 0; i < nelems; i++)
+        {
+            message[i] = my_pe;
+        }
+    }
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_broadcast_van_de_geijn(message, message, nelems, 1, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void small_reduce()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 256;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    for(int i = 0; i < nelems; i++)
+    {
+        message[i] = my_pe;
+    }
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_reduce_sum_tree(message, message, nelems, 1, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void large_reduce()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 25600;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    for(int i = 0; i < nelems; i++)
+    {
+        message[i] = my_pe;
+    }
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_reduce_sum_rabenseifner(message, message, nelems, 1, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void small_reduce_all()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 256;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    if(my_pe == 0)
+    {
+        for(int i = 0; i < nelems; i++)
+        {
+            message[i] = my_pe;
+        }
+    }
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_reduce_sum_tree(message, message, nelems, 1);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void large_reduce_all()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 25600;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    for(int i = 0; i < nelems; i++)
+    {
+        message[i] = my_pe;
+    }
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_reduce_sum_rabenseifner(message, message, nelems, 1);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void small_scatter()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 256;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    if(my_pe == 0)
+    {
+        for(int i = 0; i < nelems; i++)
+        {
+            message[i] = my_pe;
+        }
+    }
+
+    int pe_msg_sz[8] = [32, 32, 32, 32, 32, 32, 32, 32];
+    nt pe_disp[8] = [0, 32, 64, 96, 128, 160, 192, 224];
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_scatter_tree(message, message, pe_msg_sz, pe_disp, nelems, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void large_scatter()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 25600;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    if(my_pe == 0)
+    {
+        for(int i = 0; i < nelems; i++)
+        {
+            message[i] = my_pe;
+        }
+    }
+
+    int pe_msg_sz[8] = [3200, 3200, 3200, 3200, 3200, 3200, 3200, 3200];
+    nt pe_disp[8] = [0, 3200, 6400, 9600, 12800, 16000, 19200, 22400];
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_scatter_tree(message, message, pe_msg_sz, pe_disp, nelems, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void small_gather()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 256;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    for(int i = 0; i < nelems; i++)
+    {
+        message[i] = my_pe;
+    }
+
+    int pe_msg_sz[8] = [32, 32, 32, 32, 32, 32, 32, 32];
+    nt pe_disp[8] = [0, 32, 64, 96, 128, 160, 192, 224];
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_gather_tree(message, message, pe_msg_sz, pe_disp, nelems, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void large_gather()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 25600;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    for(int i = 0; i < nelems; i++)
+    {
+        message[i] = my_pe;
+    }
+
+    int pe_msg_sz[8] = [3200, 3200, 3200, 3200, 3200, 3200, 3200, 3200];
+    nt pe_disp[8] = [0, 3200, 6400, 9600, 12800, 16000, 19200, 22400];
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_gather_tree(message, message, pe_msg_sz, pe_disp, nelems, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void small_gather_all()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 256;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    for(int i = 0; i < nelems; i++)
+    {
+        message[i] = my_pe;
+    }
+
+    int pe_msg_sz[8] = [32, 32, 32, 32, 32, 32, 32, 32];
+    nt pe_disp[8] = [0, 32, 64, 96, 128, 160, 192, 224];
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_gather_all_bruck_concat(message, message, pe_msg_sz, pe_disp, nelems);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
+
+void large_gather_all()
+{
+    xbrtime_init();
+    int my_pe = xbrtime_mype();
+    int numpes = xbrtime_num_pes();
+    int nelems = 25600;
+    int *message = (int*) xbrtime_malloc(sizeof(int) * nelems);
+
+    for(int i = 0; i < nelems; i++)
+    {
+        message[i] = my_pe;
+    }
+
+    int pe_msg_sz[8] = [3200, 3200, 3200, 3200, 3200, 3200, 3200, 3200];
+    nt pe_disp[8] = [0, 3200, 6400, 9600, 12800, 16000, 19200, 22400];
+
+    xbrtime_barrier();
+
+    (*FLAG) = 1;
+
+    xbrtime_int_gather_all_ring(message, message, pe_msg_sz, pe_disp, nelems, 0);
+
+    (*FLAG) = 0;
+
+    xbrtime_barrier();
+
+    xbrtime_close();
+}
